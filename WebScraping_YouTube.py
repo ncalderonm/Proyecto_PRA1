0c0bffaafa109424a9afcc29f5e30106beb8ed5a
ncalderonm <ncalderonm@uoc.edu>
2022-04-11 18:33:52 +0200

    commite

--- WebScraping_YouTube.py
+++ WebScraping_YouTube.py
@@ -1,4 +1,4 @@
-# Imports
+# Imports:
 from selenium import webdriver
 import requests
 from selenium.webdriver.chrome.service import Service
@@ -16,7 +16,7 @@ import os
 from bs4 import BeautifulSoup
 
 
-# URL
+# URLs:
 URL = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
 URL_principal = 'https://www.youtube.com/'
 
@@ -30,11 +30,11 @@ def robots(url):
     rst = requests.get(r_link + 'robots.txt')
     return rst.text
 
-# Lo imprimimos:
+# Lo imprimimos y mostramos por pantalla:
 print(robots(URL_principal))
 
 
-# Analizamos y visualizamos el sitemap:
+# Analizamos y visualizamos el sitemap de "trends":
 sitemap = "https://www.youtube.com/trends/sitemap.xml"
 
 response = requests.get(URL)
@@ -43,16 +43,18 @@ with open('./sitemap.xml', 'wb') as file:
 
 xml_sm = "./sitemap.xml"
 
+# Opción 1:
+with open('./sitemap.xml', 'rb') as file:
+    xml = file.read()
+    xml = BeautifulSoup(xml, "html.parser")
+    xml_p = xml.prettify()
+    print(xml_p)
+
+# Opción 2:
 xml = BeautifulSoup(open(xml_sm, encoding="utf8"), "lxml")
 xml_content = xml.prettify()
 print(xml_content)
 
-'''
-import xml.dom.minidom
-
-dom = xml.dom.minidom.parse(file='../sitemap.xml')
-pretty_xml_as_string = dom.toprettyxml()
-print(pretty_xml_as_string)'''
 
 # Tecnología usada:
 import builtwith
@@ -67,15 +69,15 @@ import whois
 print(whois.whois(URL_principal))
 
 
-
-# Credentials:
+# Usuario y contraseña para nuestra cuenta Youtube y de Zenodo (Credentials):
 user = 'tipologia.uoc.2022@gmail.com'
 password = 'TIPOPRA1'
 
-# Abrir el navegador con la URL
+# Abrir el navegador con la URL:
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 
+# Llamamos a la dirección y maximizamos la ventana:
 driver.get(URL)
 sleep(1)
 driver.maximize_window()
@@ -106,22 +108,15 @@ next_button.click()
 sleep(5)
 
 
-
-# Obtener los videos
+# Obtenemos los diferentes videos:
 elements = driver.find_elements_by_xpath("""//*[@id="thumbnail"]""")
 videos = []
 for element in elements:
-    videos.append(element.get_attribute("href"))
-
-    # time.sleep(1)
-
-videos.pop(0)
-
-# Obtener información de cada video
-# diccionario = []
-data = pd.DataFrame(columns=["Url_Video", "Title", "Trending_Position", "Visualizations",
-                             "Date", "Likes", "Subscribers", "Channel", "Url_Channel", "Comments",
-                             "Type", "Length"])
+    if element.get_attribute("href") == None:
+        pass
+    else:
+        videos.append(element.get_attribute("href"))
+print(videos)
 
 # Función para generación del dataset del top n de videos, por defecto top10:
 def top_videos(n = 10):
@@ -136,31 +131,34 @@ def top_videos(n = 10):
             driver.get(video)
             time_video
 
-            # Pausar video:
-            video2 = driver.find_element_by_id('movie_player')
-            video2.send_keys(Keys.SPACE)  # hits space
+            try:
 
-            # time_video
-            path_titulo = '//*[@id="title"]/h1/yt-formatted-string'
-            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, path_titulo))).text
+                # Obtenemos los distintos atributos:
+            
+                # Pausar video:
+                video2 = driver.find_element_by_id('movie_player')
+                video2.send_keys(Keys.SPACE)  # hits space
 
-            # Obtenemos el título:
-            title = driver.find_element(by=By.XPATH, value="""//*[@id="title"]/h1/yt-formatted-string""").text
+                # Daremos tiempo a que se carguen los elementos para extraer información:
+                path_titulo = '//*[@id="title"]/h1/yt-formatted-string'
+                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, path_titulo))).text
+                title = driver.find_element(by=By.XPATH, value="""//*[@id="title"]/h1/yt-formatted-string""").text
+                tipo = driver.find_element(by=By.XPATH, value="""//*[@id="watch7-content"]/meta[16]""").get_attribute("content")
+                position = position + 1
+                visualizations = driver.find_element(by=By.XPATH, value="""//*[@id="formatted-snippet-text"]/span[1]""").text
+                date = driver.find_element(by=By.XPATH, value="""//*[@id="formatted-snippet-text"]/span[3]""").text
+                likes = driver.find_element(by=By.XPATH,
+                                                value="""/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-toggle-button-renderer[1]/a/yt-formatted-string""").get_attribute("aria-label")
+                subscribers = driver.find_element(by=By.XPATH, value="""//*[@id="owner-sub-count"]""").text
+                channel = driver.find_element(by=By.XPATH, value="""//*[@id="text"]/a""").text
+                url_channel = driver.find_element(by=By.XPATH, value="""//*[@id="text"]/a""").get_attribute("href")
+                comments = driver.find_element(by=By.XPATH, value="""//*[@id="count"]""").text
+                tipo = driver.find_element(by=By.XPATH, value="""//*[@id="watch7-content"]/meta[16]""").get_attribute("content")
+                length = driver.find_element(by=By.XPATH, value="""//*[@id="watch7-content"]/meta[6]""").get_attribute("content")
+                label = driver.find_element(by=By.XPATH, value="""//*[@id="super-title"]/a""").text
 
-            position = position + 1
-            visualizations = driver.find_element(by=By.XPATH, value="""//*[@id="formatted-snippet-text"]/span[1]""").text
-            date = driver.find_element(by=By.XPATH, value="""//*[@id="formatted-snippet-text"]/span[3]""").text
-            likes = driver.find_element(by=By.XPATH,
-                                        value="""/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-toggle-button-renderer[1]/a/yt-formatted-string""").get_attribute(
-                "aria-label")
-            subscribers = driver.find_element(by=By.XPATH, value="""//*[@id="owner-sub-count"]""").text
-
-            channel = driver.find_element(by=By.XPATH, value="""//*[@id="text"]/a""").text
-            url_channel = driver.find_element(by=By.XPATH, value="""//*[@id="text"]/a""").get_attribute("href")
-            comments = driver.find_element(by=By.XPATH, value="""//*[@id="count"]""").text
-            tipo = driver.find_element(by=By.XPATH, value="""//*[@id="watch7-content"]/meta[16]""").get_attribute("content")
-            length = driver.find_element(by=By.XPATH, value="""//*[@id="watch7-content"]/meta[6]""").get_attribute("content")
-            label = driver.find_element(by=By.XPATH, value="""//*[@id="super-title"]/a""").text
+            except Exception:
+                continue
 
             data = data.append({"Url_Video": video, "Title": title,
                                 "Trending_Position": position, "Visualizations": visualizations,
@@ -188,5 +186,5 @@ def csv_file(df):
             except Exception:
                 continue
 
-# La ejecutamos:
+# La ejecutamos para generar el CSV:
 csv_file(data)
